80d1b31b66ec4d77fcb8700af73a2debd467924b
yangshihua <100102619+yangshihua@users.noreply.github.com>
2023-09-01 16:18:08 +0800

    Update SettingAction.tsx

--- src/components/SettingAction.tsx
+++ src/components/SettingAction.tsx
@@ -66,8 +66,8 @@ export default function SettingAction(props: {
               }}
             >
               <option value="gpt-3.5-turbo">gpt-3.5-turbo(4k)</option>
+              <option value="gpt-3.5-turbo-16k">gpt-3.5-turbo(16k)</option>
               <option value="gpt-4">gpt-4(8k)</option>
-              <option value="gpt-4-32k">gpt-4(32k)</option>
             </select>
           </SettingItem>
           <SettingItem icon="i-carbon:user-online" label="系统角色指令">
